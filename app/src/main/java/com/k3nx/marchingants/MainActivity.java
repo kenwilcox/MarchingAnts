bae36620915c1172d221a2fb80a163f5d9ae1fa4
Ken Wilcox <wilcoxk@gmail.com>
2015-01-09 16:54:45 -0700

    Initial commit

---
+++ app/src/main/java/com/k3nx/marchingants/MainActivity.java
@@ -0,0 +1,47 @@
+package com.k3nx.marchingants;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private final static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Just Logging some lyrics to the Marching Ants song
+        for (int i = 0; i <= 10; i++) {
+            Log.d(TAG, "Ants go marching " + i + " by " + i + ". Hoorah! Hoorah!");
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
